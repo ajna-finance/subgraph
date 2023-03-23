bb3ed7e7e8c647e614089f1c0c047ce636802043
Ed Noepel <ed@noepel.net>
2023-03-22 21:50:03 -0400

    deployable ABIs

--- copy-abis.sh
+++ copy-abis.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-ABI_PATH="${1:-../sdk/packages/sdk-api/src/abis}"
+ABI_PATH="${1:-../contracts/forge_out}"
+
+# This script generates ABIs from forge output.
+# Before running, ensure forge output in your ABI_PATH has been compiled from the 
+# appropriate branch and commit.
 
 rm abis/*
-cp "${ABI_PATH}/ERC20.json" abis/
-cp "${ABI_PATH}/ERC20Pool.json" abis/
-cp "${ABI_PATH}/ERC20PoolFactory.json" abis/
-cp "${ABI_PATH}/ERC721PoolFactory.json" abis/
-cp "${ABI_PATH}/PoolInfoUtils.json" abis/
-cp "${ABI_PATH}/PositionManager.json" abis/
-cp "${ABI_PATH}/RewardsManager.json" abis/
+
+jq '.abi' "${ABI_PATH}/ERC20.sol/ERC20.json" > abis/ERC20.json
+jq '.abi' "${ABI_PATH}/ERC20Pool.sol/ERC20Pool.json" > abis/ERC20Pool.json
+jq '.abi' "${ABI_PATH}/ERC20PoolFactory.sol/ERC20PoolFactory.json" > abis/ERC20PoolFactory.json
+jq '.abi' "${ABI_PATH}/ERC721PoolFactory.sol/ERC721PoolFactory.json" > abis/ERC721PoolFactory.json
+jq '.abi' "${ABI_PATH}/PoolInfoUtils.sol/PoolInfoUtils.json" > abis/PoolInfoUtils.json
+jq '.abi' "${ABI_PATH}/PositionManager.sol/PositionManager.json" > abis/PositionManager.json
+jq '.abi' "${ABI_PATH}/RewardsManager.sol/RewardsManager.json" > abis/RewardsManager.json
