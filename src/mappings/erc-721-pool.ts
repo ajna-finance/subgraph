43632436084543fd8271bec3bbb713bb45bd0916
Ed Noepel <ed@noepel.net>
2023-10-05 13:21:36 -0400

    remove calldata logging

--- src/mappings/erc-721-pool.ts
+++ src/mappings/erc-721-pool.ts
@@ -1,4 +1,4 @@
-import { ByteArray, Bytes, ethereum, log } from "@graphprotocol/graph-ts"
+import { ByteArray, Bytes, ethereum } from "@graphprotocol/graph-ts"
 import {
   AddCollateralNFT as AddCollateralNFTEvent,
   AddQuoteToken as AddQuoteTokenEvent,
@@ -355,7 +355,6 @@ export function handleMergeOrRemoveCollateralNFT(
 
   // use transaction metadata to access the list of removalIndexes
   const dataWithoutSelector = event.transaction.input.subarray(4)
-  log.warning("mergeOrRemove event.transaction.input {}", [event.transaction.input.toHexString()])
   //prepend a "tuple" prefix (function params are arrays, not tuples)
   const tuplePrefix = ByteArray.fromHexString(
     '0x0000000000000000000000000000000000000000000000000000000000000020'
