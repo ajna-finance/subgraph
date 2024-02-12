d70e94d5df9f370c915a2648a9e89be6a5a305bf
Tim Reznichenko <tim.reznich@gmail.com>
2024-02-12 13:21:53 +0100

    feat: add gnosis addresses

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -34,6 +34,7 @@ poolInfoUtilsAddressTable.set('arbitrum-one', Address.fromString('0x8a7F5aFb7E3c
 poolInfoUtilsAddressTable.set('base', Address.fromString('0x97fa9b0909C238D170C1ab3B5c728A3a45BBEcBa'))
 poolInfoUtilsAddressTable.set('matic', Address.fromString('0x519021054846cd3D9883359B593B5ED3058Fbe9f'))
 poolInfoUtilsAddressTable.set('optimism', Address.fromString('0xdE6C8171b5b971F71C405631f4e0568ed8491aaC'))
+poolInfoUtilsAddressTable.set('gnosis', Address.fromString('0x2baB4c287cF33a6eC373CFE152FdbA299B653F7D'))
 poolInfoUtilsAddressTable.set('goerli', Address.fromString('0xdE8D83e069F552fbf3EE5bF04E8C4fa53a097ee5'))
 poolInfoUtilsAddressTable.set('ganache', Address.fromString('0x6c5c7fD98415168ada1930d44447790959097482'))
 export const poolInfoUtilsMulticallAddressTable = new TypedMap<string, Address>()
@@ -42,6 +43,7 @@ poolInfoUtilsMulticallAddressTable.set('arbitrum-one', Address.fromString('0xCca
 poolInfoUtilsMulticallAddressTable.set('base', Address.fromString('0x249BCE105719Ae4183204371697c2743800C225d'))
 poolInfoUtilsMulticallAddressTable.set('matic', Address.fromString('0xe6F4d9711121e5304b30aC2Aae57E3b085ad3c4d'))
 poolInfoUtilsMulticallAddressTable.set('optimism', Address.fromString('0x33e5c2b41e915acFC268a4eaACC567f612f96601'))
+poolInfoUtilsMulticallAddressTable.set('gnosis', Address.fromString('0x6A91429425Fb992A9cf300aD215e4469d0D1A75A'))
 poolInfoUtilsMulticallAddressTable.set('goerli', Address.fromString('0x63feF8659ECdC4F909ddFB55a8B701957115B906'))
 poolInfoUtilsMulticallAddressTable.set('ganache', Address.fromString('0x6548dF23A854f72335902e58a1e59B50bb3f11F1'))
 export const positionManagerAddressTable = new TypedMap<string, Address>()
@@ -50,6 +52,7 @@ positionManagerAddressTable.set('arbitrum-one', Address.fromString('0x9A0BE97153
 positionManagerAddressTable.set('base', Address.fromString('0x59710a4149A27585f1841b5783ac704a08274e64'))
 positionManagerAddressTable.set('matic', Address.fromString('0xb8DA113516bfb986B7b8738a76C136D1c16c5609'))
 positionManagerAddressTable.set('optimism', Address.fromString('0x72bF565f2BdA43294C6cC2BfE17C7FaE5258F819'))
+positionManagerAddressTable.set('gnosis', Address.fromString('0x173b32A07b41296909d7972421d63BAbA5160B37'))
 positionManagerAddressTable.set('goerli', Address.fromString('0x7b6C6917ACA28BA790837d41e5aA4A49c9Ad4570'))
 positionManagerAddressTable.set('ganache', Address.fromString('0xdF7403003a16c49ebA5883bB5890d474794cea5a'))
 
